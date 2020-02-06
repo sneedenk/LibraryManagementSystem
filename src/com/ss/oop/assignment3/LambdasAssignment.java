bde30e60199786a29013a7b2fe221bc8b79b7f0f
sneedenk <sneedenk@pm.me>
2020-02-06 18:59:36 -0500

    update lms

---
+++ src/com/ss/oop/assignment3/LambdasAssignment.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.ss.oop.assignment3;
+
+/**
+ * @author UCI
+ *
+ */
+public class LambdasAssignment {
+
+	/**
+	 * 
+	 */
+	public LambdasAssignment() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String[] strings = {"str1", "aardv", "middle", "begin", "end", "the longest"};
+		
+	}
+
+}
